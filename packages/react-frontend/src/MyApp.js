64baa5b94fc9f8259cb23ebcef8f46a0a5706882
Keith Lee <redkiinjle@gmail.com>
2023-10-08 20:54:56 -0700

    finished linking frontend and backend

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
@@ -25,14 +25,67 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index
-    });
-    setCharacters(updated);
+    const characterToDelete = characters[index];
+
+    if (!characterToDelete.id) {
+      console.error("User ID is missing. Cannot delete.");
+      return;
+    }
+
+    fetch(`http://localhost:8000/users/${characterToDelete.id}`, { method: 'DELETE' })
+      .then(response => {
+        if (response.status === 204) { 
+          const updatedCharacters = characters.filter((character, i) => i !== index);
+          setCharacters(updatedCharacters);
+        } else if (response.status === 404) {
+          console.error("Resource not found. Delete failed.");
+        } else {
+          console.error("Delete failed with status: " + response.status);
+        }
+      })
+      .catch(error => {
+        console.error("Error deleting character:", error);
+      });
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((response) => response.json())
+      .then((newUser) => {
+        if (newUser && newUser.id) {
+          setCharacters([...characters, newUser]);
+        } else {
+          console.error("Failed to insert user.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, []);
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
   }
 
   return (
