04dea2dc6df23994fc80d968acf5630cea213213
Keith Lee <redkiinjle@gmail.com>
2023-10-08 19:33:54 -0700

    added get user by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,10 +39,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/users', (req, res) => {
-  res.send(users);
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });      
@@ -51,6 +47,11 @@ const findUserByName = (name) => {
   return users['users_list']
     .filter((user) => user['name'] === name);
 }
+
+const findUserById = (id) =>
+  users['users_list']
+    .find((user) => user['id'] === id);
+
 
 app.get('/users', (req, res) => {
   const name = req.query.name;
@@ -61,5 +62,15 @@ app.get('/users', (req, res) => {
   }
   else {
     res.send(users);
+  }
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params['id']; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send('Resource not found.');
+  } else {
+    res.send(result);
   }
 });
